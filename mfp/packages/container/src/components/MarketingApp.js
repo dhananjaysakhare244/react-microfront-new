faf2ca4faa2a44ab9b71c83ea7461285e65ee7ad
dsagm7 <dsagm7>
2025-01-03 18:10:45 +0530

    communicate from sub app to container

--- mfp/packages/container/src/components/MarketingApp.js
+++ mfp/packages/container/src/components/MarketingApp.js
@@ -1,13 +1,20 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
-
+import { useHistory } from "react-router-dom";
 // use this generic way to integrate mount function from subapp.
 // this ensures that it is losely coupled from the framework that is being used in subapp.
 // this is react e.g. We can use any framework with similar structure to call mount()
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      // used for maintaining history in sub-app
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathName) history.push(nextPathName);
+      },
+    });
   });
   return <div ref={ref} />;
 };
--- mfp/packages/marketing/src/bootstrap.js
+++ mfp/packages/marketing/src/bootstrap.js
@@ -1,14 +1,20 @@
 import ReactDOM from "react-dom";
 import React from "react";
 import App from "./App";
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+import { createMemoryHistory } from "history";
+
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+
+  //send out a notification to container that navigation happened in marketing
+  if (onNavigate) history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
 };
 
 // we are running locally on dev mode
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, {});
 }
 
 // we are running through container
