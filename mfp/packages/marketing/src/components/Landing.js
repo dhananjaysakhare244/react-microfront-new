ecc8f7829c96fe6e52075a0a85a70e1c502f8d24
Babita Kumari <163977790+Babita-27@users.noreply.github.com>
2026-05-19 21:38:11 +0530

    Update Landing.js

--- mfp/packages/marketing/src/components/Landing.js
+++ mfp/packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-             Hello World
+             Home Page
             </Typography>
             <Typography
               variant="h5"
